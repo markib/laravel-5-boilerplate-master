4dde74e0304270bee76fa9b478b8b5f3a968681a
bikram <emarkib@gmail.com>
2016-09-14 17:19:56 +0545

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	resources/views/backend/access/notice/index.blade.php

---
+++ app/Forms/NoticeForm.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Forms;
+
+use Kris\LaravelFormBuilder\Form;
+
+class NoticeForm extends Form
+{
+    public function buildForm()
+    {
+        $this
+            ->add('notice_title', 'text')
+            ->add('notice_name', 'textarea')
+            ->add('publish', 'checkbox')
+            ->add('submit', 'submit', ['label' => 'Save']);
+    }
+}
